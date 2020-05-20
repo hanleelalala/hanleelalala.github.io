fe260dc2a23c7281627626403a245146edd1209e
Han Lee <hanleela@umich.edu>
2020-05-19 23:32:39 -0400

    add super dope effect!

--- js/fade_inout.js
+++ js/fade_inout.js
@@ -1,15 +1,28 @@
 $(document).ready(function(){
 	$(document).scroll(function(){ //scroll表示只要有上下滑動就會觸動這個event
 		var y = $(this).scrollTop(); //這裡面的東西表示 只要有上下滑動就做{}裡面的事
-		
-		var $horizontal = $("#AboutMePosition").position().left;
 		// document.getElementById("show_ScrollTop").innerHTML = y;
 		if (y>5){
-			$('#AboutMePosition').fadeIn(400);
-			$('#AboutMePosition').slideDown(400);
-			$('#AboutMePosition').css("right",Math.max(340-y,320))
-		}else{
-			$('#AboutMePosition').fadeOut(400);
+			$('#AboutMePosition').addClass("is-visible")
+		}
+		if (y>100){
+			$('#DTPosition').addClass("is-visible")
+		}
+		if (y>400){
+			$('#BikeeskyPosition').addClass("is-visible")
+		}
+		if (y>950){
+			$('#ExchangeStudentPosition').addClass("is-visible")
+		}
+		if (y>1280){
+			$('#ESEPosition').addClass("is-visible")
+		}
+		if (y>1400){
+			$('#RAPosition').addClass("is-visible")
+		}
+		if (y>2000){
+			$('#HDRPosition').addClass("is-visible")
 		}
 	});
 });
+
