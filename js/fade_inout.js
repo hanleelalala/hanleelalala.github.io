728283d4ad614ef0b366340ccd8628c86826a6b6
Han Lee <hanleela@umich.edu>
2020-05-20 16:45:06 -0400

    add parallax and it is cool!!

--- js/fade_inout.js
+++ js/fade_inout.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$(document).scroll(function(){ //scroll表示只要有上下滑動就會觸動這個event
 		var y = $(this).scrollTop(); //這裡面的東西表示 只要有上下滑動就做{}裡面的事
 		// document.getElementById("show_ScrollTop").innerHTML = y;
-		if (y>5){
+		if (y>-1){
 			$('#AboutMePosition').addClass("is-visible")
 		}
 		if (y>100){
@@ -23,6 +23,7 @@ $(document).ready(function(){
 		if (y>2000){
 			$('#HDRPosition').addClass("is-visible")
 		}
+
 	});
 });
 
